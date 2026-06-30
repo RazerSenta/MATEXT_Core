d80c983564a60c1ba1ee183be6ebca93b2f94411
RazerSenta <RazerSenta@proton.me>
2026-06-30 21:46:21 +0700

    add third-person mode, JSON cache, and redesign robot cat
    >w<

--- matext-core/player.gd
+++ matext-core/player.gd
@@ -10,29 +10,26 @@ const JUMP_VELOCITY = 4.5
 @onready var crosshair = $screen/crosshair
 @onready var miaw = $CollisionShape3D/cat/miaw
 var camera_dir = Vector3.ZERO
-var camera_mode_dir = Vector3.ZERO
 var camera_sens = 0.2
 var camera_fov = 75
 var camera_fov_zoom = 10
 var camera_move = true
 var camera_mode = 0
+var view_mode_dir = Vector3.ZERO
 
 var player_sprint = false
 var player_zoom = false
 var inventory = {'money':200}
 
-#variable for mobile joystick and screen touch
 var joystick_direction = Vector2.ZERO
 var joystick_center = Vector2.ZERO
 var joystick_active = false
 var joystick_touch = -1
 var camera_touch = -1
 
-#variable for save status and name from object scanner
 var scanner_id_get = {}
 var scanner_object = ''
 
-#wow terminal
 var terminal_costum_display = ''
 var terminal_open = ''
 var terminal_on = false
@@ -48,52 +45,17 @@ var terminal_help = {
 	'press [Alt+q]' : 'exit the game',
 	'hold [x]' : 'enable cursor'}
 
-func json_conv(title, data):
-	var output = ['| ', title, ' >_\nv\n']
-	for item in data:
-		output.append('• ' + item + ' : ' + str(data[item]) + '\n')
-	return ''.join(output)
-func get_input_direction():
-	if Input.is_action_just_pressed("ui_left"):
-		return 'left'
-	elif Input.is_action_just_pressed("ui_right"):
-		return 'right'
-	elif Input.is_action_just_pressed("ui_up"):
-		return 'up'
-	elif Input.is_action_just_pressed("ui_down"):
-		return 'down'
-func get_input_terminal():
-	if Input.is_action_just_pressed("ui_up"):
-		terminal_open = 'inventory'
-	elif Input.is_action_just_pressed("ui_right"):
-		terminal_open = 'object_scanner'
-	elif Input.is_action_just_pressed("ui_down"):
-		terminal_open = 'help'
-	elif Input.is_action_just_pressed("click"):
-		terminal_open = 'terminal_area'
-func get_input_action():
-	if Input.is_action_just_pressed("sprint"):
-		player_sprint = !player_sprint
-	elif Input.is_action_just_pressed("open_terminal"):
-		terminal_on = !terminal_on
-	elif Input.is_action_just_pressed("respawn"):
-		self.global_position = Vector3.ZERO
-	elif Input.is_action_just_pressed("exit"):
-		get_tree().quit()
-	elif Input.is_action_just_pressed("zoom"):
-		player_zoom = !player_zoom
-		camera_set.fov = 10 if player_zoom else camera_fov
-		camera_sens = 0.05 if player_zoom else 0.2
-	elif Input.is_action_just_pressed("view_mode"):
-		camera_mode += 1
-		if camera_mode == 1:
-			camera_set.position.z = 5
-			miaw.visible = true
-		elif camera_mode == 2:
-			camera_set.position.z = 0
-			camera_handle.rotation_degrees.y = 0
-			camera_mode = 0
-			miaw.visible = false
+var json_conv_cache = {}
+func json_conv(title, data, get_in_cache):
+	if get_in_cache and [title, data] in json_conv_cache:
+		return json_conv_cache[[title, data]]
+	else:
+		var output = ['| ', title, ' >_\nv\n']
+		for item in data:
+			output.append('• ' + item + ' : ' + str(data[item]) + '\n')
+		if get_in_cache:
+			json_conv_cache[[title, data]] = ''.join(output)
+		return ''.join(output)
 
 func terminal_system():
 	if terminal_on:
@@ -111,14 +73,15 @@ func terminal_system():
 					scanner_id_get = {}
 			else:
 				scanner_id_get = {}
-			terminal_display.text = json_conv("OBJECT SCANNER", scanner_id_get['status'] if 'status' in scanner_id_get else '') + ('\n________________________________,,,\n' + '• press the object to enter its terminal' if 'menu' in scanner_id_get else '')
-			terminal_open = 'terminal_access' if 'menu' in scanner_id_get and Input.is_action_just_pressed("click") else 'object_scanner'
+			terminal_display.text = json_conv("OBJECT SCANNER", scanner_id_get['status'] if 'status' in scanner_id_get else '', false) + ('\n________________________________,,,\n' + '• press the object to enter its terminal' if 'menu' in scanner_id_get else '')
+			if 'menu' in scanner_id_get and Input.is_action_just_pressed("click"):
+				terminal_open = 'terminal_access' 
 
 		elif terminal_open == 'help':
-			terminal_display.text = json_conv("TERMINAL HELP", terminal_help)
+			terminal_display.text = json_conv("TERMINAL HELP", terminal_help, true)
 
 		elif terminal_open == 'terminal_access':
-			terminal_display.text = json_conv("TERMINAL ACCESS", scanner_id_get['menu'])
+			terminal_display.text = json_conv("TERMINAL ACCESS", scanner_id_get['menu'], true)
 			if scanner_object.has_method('terminal'):
 				var output = scanner_object.terminal(get_input_direction(), self)
 				if output:
@@ -129,12 +92,12 @@ func terminal_system():
 						terminal_open = 'display_costum'
 
 		elif terminal_open == 'object_status':
-			terminal_display.text = json_conv("OBJECT STATUS", scanner_id_get['status'] if 'status' in scanner_id_get else '')
+			terminal_display.text = json_conv("OBJECT STATUS", scanner_id_get['status'] if 'status' in scanner_id_get else '', false)
 
 		elif terminal_open == 'display_costum':
 			terminal_display.text = terminal_costum_display
 		else:
-			terminal_display.text = json_conv('WELCOME TO THE TERMINAL', terminal_menu)
+			terminal_display.text = json_conv('WELCOME TO THE TERMINAL', terminal_menu, true)
 			get_input_terminal()
 	else:
 		terminal_display.text = ''
@@ -169,24 +132,42 @@ func camera_input(event):
 		camera_dir.x -= event.relative.y * camera_sens
 		camera_dir.y -= event.relative.x * camera_sens
 		camera_dir.x = clamp(camera_dir.x, -75, 90)
-		camera_mode_dir.y -= event.relative.x * camera_sens
+		view_mode_dir.y -= event.relative.x * camera_sens
 		if camera_dir.y > 360 or camera_dir.y < -360:
 			camera_dir.y = 0
-	#sprint effect
-	if not player_zoom:
-		camera_set.fov = camera_fov + 10 if player_sprint and Input.is_action_pressed("ui_up") else camera_fov
+		elif view_mode_dir.y > 360 or view_mode_dir.y < -360:
+			view_mode_dir.y = 0
+	if camera_mode == 1 or camera_mode == 2:
+		if event is InputEventMouseButton and event.pressed:
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				camera_set.position.z += 1
+			elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				camera_set.position.z -= 1
+		elif camera_set.position.z == 0:
+			camera_mode = 3
 func camera():
 	if camera_move:
 		camera_handle.rotation_degrees.x = camera_dir.x
-		if camera_mode == 1:
+		if camera_mode == 2:
 			if Input.is_action_pressed("ui_up"):
 				self.rotation_degrees.y = camera_dir.y
-				camera_mode_dir.y = 0
-				camera_handle.rotation_degrees.y = camera_mode_dir.y
+				view_mode_dir.y = 0
+				camera_handle.rotation_degrees.y = view_mode_dir.y
 			else:
-				camera_handle.rotation_degrees.y = camera_mode_dir.y
+				camera_handle.rotation_degrees.y = view_mode_dir.y
 		else:
 			self.rotation_degrees.y = camera_dir.y
+			view_mode_dir.y = 0
+
+	if not player_zoom:
+		camera_set.fov = camera_fov + 10 if player_sprint and Input.is_action_pressed("ui_up") else camera_fov
+
+	if camera_mode == 3:
+		camera_set.position.z = 0
+		camera_handle.rotation_degrees.y = 0
+		miaw.visible = false
+		camera_mode = 0
+
 func respawn_from_void():
 	if self.global_position.y <= -15.0 and camera_dir.x <= 10 and camera_dir.x >= -10: 
 		self.global_position = Vector3(0, 100, 0)
@@ -194,16 +175,50 @@ func respawn_from_void():
 func cursor_visible():
 	camera_move = false if Input.is_action_pressed("cursor_enable") else true
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if Input.is_action_pressed("cursor_enable") else Input.MOUSE_MODE_CAPTURED
-func crosshair_is_visible():
+func crosshair_visible():
 	if Input.is_action_pressed("cursor_enable") or player_zoom:
 		crosshair.visible = false
 	elif player_zoom:
 		crosshair.visible = false
-	elif camera_mode == 1:
+	elif camera_mode == 1 or camera_mode == 2:
 		crosshair.visible = false
 	else:
 		crosshair.visible = true
 
+func get_input_direction():
+	if Input.is_action_just_pressed("ui_left"):
+		return 'left'
+	elif Input.is_action_just_pressed("ui_right"):
+		return 'right'
+	elif Input.is_action_just_pressed("ui_up"):
+		return 'up'
+	elif Input.is_action_just_pressed("ui_down"):
+		return 'down'
+func get_input_terminal():
+	if Input.is_action_just_pressed("ui_up"):
+		terminal_open = 'inventory'
+	elif Input.is_action_just_pressed("ui_right"):
+		terminal_open = 'object_scanner'
+	elif Input.is_action_just_pressed("ui_down"):
+		terminal_open = 'help'
+	elif Input.is_action_just_pressed("click"):
+		terminal_open = 'terminal_area'
+func get_input_action():
+	if Input.is_action_just_pressed("sprint"):
+		player_sprint = !player_sprint
+	elif Input.is_action_just_pressed("open_terminal"):
+		terminal_on = !terminal_on
+	elif Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("zoom"):
+		player_zoom = !player_zoom
+		camera_set.fov = 10 if player_zoom else camera_fov
+		camera_sens = 0.05 if player_zoom else 0.2
+	elif Input.is_action_just_pressed("view_mode"):
+		camera_mode += 1
+		if camera_mode == 1:
+			camera_set.position.z = 5
+			miaw.visible = true
 func _ready() -> void:
 	add_to_group("player")
 	miaw.visible = false
@@ -211,13 +226,14 @@ func _input(event: InputEvent) -> void:
 	camera_input(event)
 	get_input_action()
 	respawn_from_void()
-	crosshair_is_visible()
+	crosshair_visible()
 	cursor_visible()
 	terminal_system()
+	
 func _process(delta: float) -> void:
 	camera()
 	if terminal_open == 'inventory':
-		terminal_display.text = json_conv("INVENTORY", inventory)
+		terminal_display.text = json_conv("INVENTORY", inventory, false)
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -229,10 +245,10 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") 
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
 	if direction:
+		
 		velocity.x = direction.x * (SPRINT if player_sprint and Input.is_action_pressed("ui_up") else SPEED)
 		velocity.z = direction.z * (SPRINT if player_sprint and Input.is_action_pressed("ui_up") else SPEED)
 	else:
